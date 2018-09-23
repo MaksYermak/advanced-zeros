84fe1801252325f7abeca758e8c2ec99658f8fa0
Maksim <maks.yermakou@gmail.com>
2018-09-23 18:06:58 +0300

    My solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var zeros = number;
+	var tempBase, prime, c, tempNumb;
+	tempBase = base;
+
+	for(var i=2; i<=base; i++){
+		
+		if(tempBase%i == 0){
+			prime = 0;
+			while(tempBase%i == 0){
+				prime++;
+				tempBase /= i;
+			}
+
+			c = 0;
+			tempNumb = number;
+			while(tempNumb/i > 0){
+				c += parseInt(tempNumb/i);
+				tempNumb = parseInt(tempNumb/i);
+			}
+			zeros = Math.min(zeros, parseInt(c/prime));
+		}
+	}
+
+	return zeros;
 }
